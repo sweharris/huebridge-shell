789115a8835eb63bbf67aa2bcbca9b976dc9f117
Stephen Harris <sweharris@users.noreply.github.com>
2019-09-18 20:43:42 -0400

    Make a more complete model of the light structure reported by Hue. I don't know if this is necessary or not, but it makes the JSON ouput very very close to that provided by a real hue bridge, so it can't hurt!

--- lights.go
+++ lights.go
@@ -23,25 +23,54 @@ import (
 // to what I think is the minimum necessary
 
 // These are the values we need to track per light.
-// (Well, Reachable is always True, but it's still part of State)
-// And "name" is an internal structure that doesn't export... I hope!
 type Light_State struct {
-	On        bool `json:"on"`
-	Bri       int  `json:"bri"`
-	Reachable bool `json:"reachable"`
-	name      string
+	On   bool
+	Bri  int
+	name string
 }
 
-// Except for State, we expect these to be constant for every light.
+// This is what we return as an API call.  Most of this is constant!
 type Light struct {
-	State            Light_State `json:"state"`
-	Type             string      `json:"type"`
-	Name             string      `json:"name"`
-	ModelID          string      `json:"modelid"`
-	ManufacturerName string      `json:"manufacturername"`
-	ProductName      string      `json:"productname"`
-	UniqueID         string      `json:"uniqueid"`
-	SWVersion        string      `json:"swversion"`
+	State struct {
+		On        bool   `json:"on"`
+		Bri       int    `json:"bri"`
+		Alert     string `json:"alert"`
+		Mode      string `json:"mode"`
+		Reachable bool   `json:"reachable"`
+	} `json:"state"`
+	SWUpdate struct {
+		State       string `json:"state"`
+		LastInstall string `json:"lastinstall"`
+	} `json:"swupdate"`
+	Type             string `json:"type"`
+	Name             string `json:"name"`
+	ModelID          string `json:"modelid"`
+	ManufacturerName string `json:"manufacturername"`
+	ProductName      string `json:"productname"`
+	Capabilities     struct {
+		Certified bool `json:"certified"`
+		Control   struct {
+			MinDim int `json:"mindimlevel"`
+			MaxLum int `json:"maxlumen"`
+		} `json:"control"`
+		Streaming struct {
+			Renderer bool `json:"renderer"`
+			Proxy    bool `json:"proxy"`
+		} `json:"streaming"`
+	} `json:"capabilities"`
+	Config struct {
+		Archetype string `json:"archetype"`
+		Function  string `json:"function"`
+		Direction string `json:"direction"`
+		Startup   struct {
+			Mode       string `json:"mode"`
+			Configured bool   `json:"configured"`
+		} `json:"startup"`
+	} `json:"config"`
+	UniqueID   string `json:"uniqueid"`
+	SWConfigID string `json:"swconfigid"`
+	SWVersion  string `json:"swversion"`
+	ProductID  string `json:"productid"`
 }
 
 // We will index lights based on their name
@@ -49,10 +78,9 @@ var Lights map[string]Light_State
 
 func NewLight(name string) Light_State {
 	return Light_State{
-		On:        false,
-		Bri:       0,
-		Reachable: true,
-		name:      name,
+		On:   false,
+		Bri:  0,
+		name: name,
 	}
 }
 
@@ -67,14 +95,40 @@ func get_or_create_light(index, name string) Light_State {
 }
 
 func light_from_state(name string, state Light_State) Light {
-	return Light{
-		State:            state,
-		Type:             "Dimmable light",
-		Name:             state.name,
-		ModelID:          "LWB014",
-		ManufacturerName: "Philips",
-		ProductName:      "Hue white lamp",
-		UniqueID:         name,
-		SWVersion:        "1",
-	}
+	l := &Light{}
+
+	// These are the only things that change.  Everything else is constant
+	l.State.On = state.On
+	l.State.Bri = state.Bri
+	l.Name = state.name
+	l.UniqueID = name
+
+	l.Type = "Dimmable light"
+	l.ModelID = "LWB014"
+	l.ManufacturerName = "Philips"
+	l.ProductName = "Hue white lamp"
+	l.SWVersion = "1"
+	l.SWConfigID = "69806BE9"
+	l.ProductID = "Philips-LWB014-1-A19DLv4"
+
+	l.State.Alert = "none"
+	l.State.Mode = "homeautomation"
+	l.State.Reachable = true
+
+	l.SWUpdate.State = "noupdates"
+	l.SWUpdate.LastInstall = "2018-12-14T00:08:29"
+
+	l.Capabilities.Certified = true
+	l.Capabilities.Control.MinDim = 5000
+	l.Capabilities.Control.MaxLum = 840
+	l.Capabilities.Streaming.Renderer = false
+	l.Capabilities.Streaming.Proxy = false
+
+	l.Config.Archetype = "classicbulb"
+	l.Config.Function = "functional"
+	l.Config.Direction = "omnidirectional"
+	l.Config.Startup.Mode = "powerfail"
+	l.Config.Startup.Configured = true
+
+	return *l
 }
